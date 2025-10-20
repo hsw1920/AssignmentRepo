b839b03aedab60d6c00221d6d18ee559a32221ff
hsw1920 <hsw1920@gmail.com>
2025-10-21 01:38:41 +0900

    Feature: 주어진 형식의 Dummy 추가

---
+++ DeepmediAssignment/DeepmediAssignment/Sources/Entity/Dummy.swift
@@ -0,0 +1,156 @@
+//
+//  Dummy.swift
+//  DeepmediAssignment
+//
+//  Created by 홍승완 on 10/20/25.
+//
+
+import Foundation
+
+/// 1번 데이터
+let dummy1: [String: [TargetItem]] = [
+    "30 수요일": [
+        TargetItem(measureTime: "13:22:00.106+0900", value: 191),
+        TargetItem(measureTime: "13:47:07.620+0900", value: 136),
+        TargetItem(measureTime: "13:25:20.859+0900", value: 128),
+    ],
+    "29 화요일": [
+        TargetItem(measureTime: "08:24:34.695+0900", value: 129),
+        TargetItem(measureTime: "13:24:32.605+0900", value: 120)
+    ],
+    "28 월요일": [
+        TargetItem(measureTime: "21:25:22.054+0900", value: 120),
+        TargetItem(measureTime: "19:03:32.702+0900", value: 119),
+        TargetItem(measureTime: "14:41:51.466+0900", value: 125),
+        TargetItem(measureTime: "12:34:27.099+0900", value: 128),
+        TargetItem(measureTime: "12:32:59.414+0900", value: 110),
+    ],
+    "21 월요일": [
+        TargetItem(measureTime: "20:23:33.551+0900", value: 107),
+        TargetItem(measureTime: "13:47:43.472+0900", value: 139)
+    ],
+    "18 금요일": [
+        TargetItem(measureTime: "14:22:16.556+0900", value: 148),
+        TargetItem(measureTime: "13:47:07.620+0900", value: 121),
+        TargetItem(measureTime: "13:25:20.859+0900", value: 110),
+        TargetItem(measureTime: "13:10:41.985+0900", value: 122),
+        TargetItem(measureTime: "13:07:24.188+0900", value: 131),
+    ],
+    "9 수요일": [
+        TargetItem(measureTime: "14:22:16.556+0900", value: 118),
+        TargetItem(measureTime: "13:10:41.985+0900", value: 129),
+        TargetItem(measureTime: "13:07:24.188+0900", value: 123),
+    ],
+    "4 금요일": [
+        TargetItem(measureTime: "14:22:16.556+0900", value: 128),
+        TargetItem(measureTime: "13:47:07.620+0900", value: 136),
+        TargetItem(measureTime: "13:25:20.859+0900", value: 128),
+        TargetItem(measureTime: "13:10:41.985+0900", value: 125),
+        TargetItem(measureTime: "13:07:24.188+0900", value: 130),
+    ],
+]
+
+/// 2번 데이터
+let dummy2: [String: [TargetItem]] = [
+    "30 수요일": [
+        TargetItem(measureTime: "13:22:00.106+0900", value: 72),
+        TargetItem(measureTime: "13:47:07.620+0900", value: 88),
+        TargetItem(measureTime: "13:25:20.859+0900", value: 95),
+    ],
+    "29 화요일": [
+        TargetItem(measureTime: "08:24:34.695+0900", value: 103),
+        TargetItem(measureTime: "13:24:32.605+0900", value: 74)
+    ],
+    "28 월요일": [
+        TargetItem(measureTime: "21:25:22.054+0900", value: 78),
+        TargetItem(measureTime: "19:03:32.702+0900", value: 91),
+        TargetItem(measureTime: "14:41:51.466+0900", value: 105),
+        TargetItem(measureTime: "12:34:27.099+0900", value: 69),
+        TargetItem(measureTime: "12:32:59.414+0900", value: 110),
+    ],
+    "21 월요일": [
+        TargetItem(measureTime: "20:23:33.551+0900", value: 83),
+        TargetItem(measureTime: "13:47:43.472+0900", value: 92)
+    ],
+    "18 금요일": [
+        TargetItem(measureTime: "14:22:16.556+0900", value: 66),
+        TargetItem(measureTime: "13:47:07.620+0900", value: 79),
+        TargetItem(measureTime: "13:25:20.859+0900", value: 72),
+        TargetItem(measureTime: "13:10:41.985+0900", value: 95),
+        TargetItem(measureTime: "13:07:24.188+0900", value: 60),
+    ],
+    "9 수요일": [
+        TargetItem(measureTime: "14:22:16.556+0900", value: 108),
+        TargetItem(measureTime: "13:10:41.985+0900", value: 82),
+        TargetItem(measureTime: "13:07:24.188+0900", value: 91),
+    ],
+    "4 금요일": [
+        TargetItem(measureTime: "14:22:16.556+0900", value: 102),
+        TargetItem(measureTime: "13:47:07.620+0900", value: 109),
+        TargetItem(measureTime: "13:25:20.859+0900", value: 90),
+        TargetItem(measureTime: "13:10:41.985+0900", value: 97),
+        TargetItem(measureTime: "13:07:24.188+0900", value: 100),
+    ],
+]
+
+/// 3번 데이터
+let dummy3: [String: [TargetItem]] = [
+    "30 수요일": [
+        TargetItem(measureTime: "13:22:00.106+0900", value: 55),
+        TargetItem(measureTime: "13:47:07.620+0900", value: 44),
+    ],
+    "28 월요일": [
+        TargetItem(measureTime: "21:25:22.054+0900", value: 48),
+        TargetItem(measureTime: "12:34:27.099+0900", value: 42),
+        TargetItem(measureTime: "12:32:59.414+0900", value: 60),
+    ],
+    "21 월요일": [
+        TargetItem(measureTime: "20:23:33.551+0900", value: 39),
+        TargetItem(measureTime: "13:47:43.472+0900", value: 55)
+    ],
+    "18 금요일": [
+        TargetItem(measureTime: "14:22:16.556+0900", value: 26),
+        TargetItem(measureTime: "13:47:07.620+0900", value: 33),
+        TargetItem(measureTime: "13:25:20.859+0900", value: 42),
+        TargetItem(measureTime: "13:10:41.985+0900", value: 24),
+        TargetItem(measureTime: "13:07:24.188+0900", value: 29),
+    ],
+    "9 수요일": [
+        TargetItem(measureTime: "14:22:16.556+0900", value: 48),
+        TargetItem(measureTime: "13:10:41.985+0900", value: 27),
+    ],
+    "4 금요일": [
+        TargetItem(measureTime: "14:22:16.556+0900", value: 59),
+        TargetItem(measureTime: "13:47:07.620+0900", value: 51),
+        TargetItem(measureTime: "13:25:20.859+0900", value: 48),
+    ],
+]
+
+/// 4번 데이터
+let dummy4: [String: [TargetItem]] = [
+    "30 수요일": [
+        TargetItem(measureTime: "13:22:00.106+0900", value: 0.03),
+        TargetItem(measureTime: "13:47:07.620+0900", value: 0.0),
+    ],
+    "29 화요일": [
+        TargetItem(measureTime: "08:24:34.695+0900", value: 0.0)
+    ],
+    "28 월요일": [
+        TargetItem(measureTime: "21:25:22.054+0900", value: 0.6),
+        TargetItem(measureTime: "19:03:32.702+0900", value: 0.0),
+        TargetItem(measureTime: "12:32:59.414+0900", value: 0.05),
+    ],
+    "21 월요일": [
+        TargetItem(measureTime: "20:23:33.551+0900", value: 0.2)
+    ],
+    "18 금요일": [
+        TargetItem(measureTime: "14:22:16.556+0900", value: 0.15),
+        TargetItem(measureTime: "13:25:20.859+0900", value: 0.3),
+        TargetItem(measureTime: "13:10:41.985+0900", value: 0.0),
+        TargetItem(measureTime: "13:07:24.188+0900", value: 0.0),
+    ],
+    "9 수요일": [
+        TargetItem(measureTime: "14:22:16.556+0900", value: 0.0),
+        TargetItem(measureTime: "13:07:24.188+0900", value: 0.08),
+    ],
+]
---
+++ DeepmediAssignment/DeepmediAssignment/Sources/Entity/TargetItem.swift
@@ -0,0 +1,13 @@
+//
+//  TargetItem.swift
+//  DeepmediAssignment
+//
+//  Created by 홍승완 on 10/20/25.
+//
+
+import Foundation
+
+struct TargetItem {
+  let measureTime: String
+  let value: Double
+}
